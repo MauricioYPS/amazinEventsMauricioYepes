05bacbf6fce096080dead911c7e473f5de522fe3
MauricioYPS <yepes060@gmail.com>
2024-06-12 19:14:47 -0500

    Home modulado funcional

--- javaScript/home.js
+++ javaScript/home.js
@@ -1,4 +1,5 @@
-let data = {
+import {aplicarFiltros, crearTarjetas} from "../modules/modules.js";
+export let data = {
   currentDate: "2023-01-01",
   events: [
     {
@@ -196,60 +197,56 @@ let data = {
 };
 
 
-let tarjetaPadre = document.querySelector("#cardzz")
+export let tarjetaPadre = document.querySelector("#cardzz")
 
 crearTarjetas(tarjetaPadre, data.events)
 
-function crearTarjetas(tarjetaPadre, eventos) {
-  tarjetaPadre.innerHTML = '';
-  if (eventos.length === 0){
-    tarjetaPadre.innerHTML = '<h2 class="txtt">There are no matches in your search</h2>'
-    return;
-  }
-  for (let i = 0; i < eventos.length; i++) {
-    let evento = eventos[i];
+// function crearTarjetas(tarjetaPadre, eventos) {
+//   tarjetaPadre.innerHTML = '';
+//   if (eventos.length === 0){
+//     tarjetaPadre.innerHTML = '<h2 class="txtt">There are no matches in your search</h2>'
+//     return;
+//   }
+//   for (let i = 0; i < eventos.length; i++) {
+//     let evento = eventos[i];
 
-    let nuevaTarjeta = document.createElement("div");
-    nuevaTarjeta.classList.add("card");
-    nuevaTarjeta.classList.add("m-3");
-    nuevaTarjeta.classList.add("tarjetaImgs")
-    nuevaTarjeta.style.width = "18rem";
+//     let nuevaTarjeta = document.createElement("div");
+//     nuevaTarjeta.classList.add("card");
+//     nuevaTarjeta.classList.add("m-3");
+//     nuevaTarjeta.classList.add("tarjetaImgs")
+//     nuevaTarjeta.style.width = "18rem";
 
-    nuevaTarjeta.innerHTML = `<img src="${evento.image}" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">${evento.name}</h5>
-            <p class="card-text">${evento.description}</p>
-        </div>
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">Price : ${evento.price}
-                <div class="card-body marginLeftManual">
-                    <a href="/details.html?id=${evento._id}" class="btn btn-secondary">Details</a>
-                </div>
-            </li>
-        </ul>`
+//     nuevaTarjeta.innerHTML = `<img src="${evento.image}" class="card-img-top" alt="...">
+//         <div class="card-body">
+//             <h5 class="card-title">${evento.name}</h5>
+//             <p class="card-text">${evento.description}</p>
+//         </div>
+//         <ul class="list-group list-group-flush">
+//             <li class="list-group-item">Price : ${evento.price}
+//                 <div class="card-body marginLeftManual">
+//                     <a href="/details.html?id=${evento._id}" class="btn btn-secondary">Details</a>
+//                 </div>
+//             </li>
+//         </ul>`
 
-    tarjetaPadre.appendChild(nuevaTarjeta);
-  }
-    let zaza = new Array(eventos)
-    let jaja = zaza[0]
-    console.log(jaja);
-    console.log(jaja[0]._id);
-  
-} 
+//     tarjetaPadre.appendChild(nuevaTarjeta);
+//   }
 
-let selectedCategories = [];
-let searchText = '';
+// } 
 
-function aplicarFiltros() {
-  let eventosFiltrados = data.events.filter(function(evento) {
-    let cumpleCategoria = selectedCategories.length === 0 || selectedCategories.indexOf(evento.category) !== -1;
-    let cumpleTexto = evento.name.toLowerCase().includes(searchText.toLowerCase()) ||
-                      evento.description.toLowerCase().includes(searchText.toLowerCase());
-    return cumpleCategoria && cumpleTexto;
-  });
+export let selectedCategories = [];
+export let searchText = '';
 
-  crearTarjetas(tarjetaPadre, eventosFiltrados);
-}
+// function aplicarFiltros() {
+//   let eventosFiltrados = data.events.filter(function(evento) {
+//     let cumpleCategoria = selectedCategories.length === 0 || selectedCategories.indexOf(evento.category) !== -1;
+//     let cumpleTexto = evento.name.toLowerCase().includes(searchText.toLowerCase()) ||
+//                       evento.description.toLowerCase().includes(searchText.toLowerCase());
+//     return cumpleCategoria && cumpleTexto;
+//   });
+
+//   crearTarjetas(tarjetaPadre, eventosFiltrados);
+// }
 
 let divPapa = document.querySelector("#esternos");
 let categories = data.events.map(function(event) { return event.category; })
--- modules/modules.js
+++ modules/modules.js
@@ -1,1 +1,45 @@
 //empezamos por 3era vez
+import { data,searchText,selectedCategories,tarjetaPadre } from "../javaScript/home.js";
+
+export function aplicarFiltros() {
+    let eventosFiltrados = data.events.filter(function(evento) {
+      let cumpleCategoria = selectedCategories.length === 0 || selectedCategories.indexOf(evento.category) !== -1;
+      let cumpleTexto = evento.name.toLowerCase().includes(searchText.toLowerCase()) ||
+                        evento.description.toLowerCase().includes(searchText.toLowerCase());
+      return cumpleCategoria && cumpleTexto;
+    });
+  
+    crearTarjetas(tarjetaPadre, eventosFiltrados);
+  }
+  export function crearTarjetas(tarjetaPadre, eventos) {
+    tarjetaPadre.innerHTML = '';
+    if (eventos.length === 0){
+      tarjetaPadre.innerHTML = '<h2 class="txtt">There are no matches in your search</h2>'
+      return;
+    }
+    for (let i = 0; i < eventos.length; i++) {
+      let evento = eventos[i];
+  
+      let nuevaTarjeta = document.createElement("div");
+      nuevaTarjeta.classList.add("card");
+      nuevaTarjeta.classList.add("m-3");
+      nuevaTarjeta.classList.add("tarjetaImgs")
+      nuevaTarjeta.style.width = "18rem";
+  
+      nuevaTarjeta.innerHTML = `<img src="${evento.image}" class="card-img-top" alt="...">
+          <div class="card-body">
+              <h5 class="card-title">${evento.name}</h5>
+              <p class="card-text">${evento.description}</p>
+          </div>
+          <ul class="list-group list-group-flush">
+              <li class="list-group-item">Price : ${evento.price}
+                  <div class="card-body marginLeftManual">
+                      <a href="/details.html?id=${evento._id}" class="btn btn-secondary">Details</a>
+                  </div>
+              </li>
+          </ul>`
+  
+      tarjetaPadre.appendChild(nuevaTarjeta);
+    }
+  
+  }
